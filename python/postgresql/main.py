541a4df001136652a1392e893fa2b8101e85b8c9
Ian Cook <ianmcook@gmail.com>
2025-11-21 20:51:29 -0500

    Update Python PostgreSQL example

--- python/postgresql/main.py
+++ python/postgresql/main.py
@@ -13,18 +13,13 @@
 # limitations under the License.
 
 # /// script
-# requires-python = ">=3.9"
-# dependencies = ["adbc-driver-manager>=1.8.0", "pyarrow>=20.0.0"]
+# requires-python = "==3.14"
+# dependencies = ["pyarrow>=20.0.0"]
 # ///
 
 from adbc_driver_manager import dbapi
 
-with dbapi.connect(
-    driver="postgresql",
-    db_kwargs={
-        "uri": "postgresql://localhost:5432/demo"
-    }
-) as con, con.cursor() as cursor:
+with dbapi.connect("postgresql://localhost:5432/demo") as con, con.cursor() as cursor:
     cursor.execute("SELECT * FROM games;")
     table = cursor.fetch_arrow_table()
 
