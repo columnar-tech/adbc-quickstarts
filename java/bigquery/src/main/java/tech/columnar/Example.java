7fe9401218217615881883343455fbc1356c0f04
Emil Sadek <esadek@users.noreply.github.com>
2026-01-08 19:46:47 -0800

    Format Java files

--- java/bigquery/src/main/java/tech/columnar/Example.java
+++ java/bigquery/src/main/java/tech/columnar/Example.java
@@ -16,41 +16,42 @@
 
 package tech.columnar;
 
-import org.apache.arrow.adbc.driver.jni.JniDriver;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.arrow.adbc.core.AdbcConnection;
 import org.apache.arrow.adbc.core.AdbcDatabase;
 import org.apache.arrow.adbc.core.AdbcStatement;
+import org.apache.arrow.adbc.driver.jni.JniDriver;
 import org.apache.arrow.adbc.drivermanager.AdbcDriverManager;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.vector.ipc.ArrowReader;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Example {
-    private static final String DRIVER_FACTORY = "org.apache.arrow.adbc.driver.jni.JniDriverFactory";
+  private static final String DRIVER_FACTORY = "org.apache.arrow.adbc.driver.jni.JniDriverFactory";
 
-    public static void main(String[] args) throws Exception {
-        Map<String, Object> params = new HashMap<>();
-        JniDriver.PARAM_DRIVER.set(params, "bigquery");
-        params.put("adbc.bigquery.sql.project_id", "my-gcp-project");
-        params.put("adbc.bigquery.sql.dataset_id", "bigquery-public-data");
+  public static void main(String[] args) throws Exception {
+    Map<String, Object> params = new HashMap<>();
+    JniDriver.PARAM_DRIVER.set(params, "bigquery");
+    params.put("adbc.bigquery.sql.project_id", "my-gcp-project");
+    params.put("adbc.bigquery.sql.dataset_id", "bigquery-public-data");
 
-        try (BufferAllocator allocator = new RootAllocator();
-             AdbcDatabase db = AdbcDriverManager.getInstance().connect(DRIVER_FACTORY, allocator, params);
-             AdbcConnection conn = db.connect();
-             AdbcStatement stmt = conn.createStatement()) {
-            stmt.setSqlQuery("""
-              SELECT word, corpus FROM bigquery-public-data.samples.shakespeare
-              WHERE word_count = 1 ORDER BY RAND() LIMIT 5;
-            """);
-            try (AdbcStatement.QueryResult result = stmt.executeQuery()) {
-                ArrowReader reader = result.getReader();
-                while (reader.loadNextBatch()) {
-                    System.out.println(reader.getVectorSchemaRoot().contentToTSVString());
-                }
-            }
+    try (BufferAllocator allocator = new RootAllocator();
+        AdbcDatabase db =
+            AdbcDriverManager.getInstance().connect(DRIVER_FACTORY, allocator, params);
+        AdbcConnection conn = db.connect();
+        AdbcStatement stmt = conn.createStatement()) {
+      stmt.setSqlQuery(
+          """
+            SELECT word, corpus FROM bigquery-public-data.samples.shakespeare
+            WHERE word_count = 1 ORDER BY RAND() LIMIT 5;
+          """);
+      try (AdbcStatement.QueryResult result = stmt.executeQuery()) {
+        ArrowReader reader = result.getReader();
+        while (reader.loadNextBatch()) {
+          System.out.println(reader.getVectorSchemaRoot().contentToTSVString());
         }
+      }
     }
+  }
 }
