663f96cb5099ec39cb6b394af37128a3fd3deefe
Emil Sadek <esadek@hotmail.com>
2026-01-12 12:00:07 -0800

    Format Java files (#63)
    Co-authored-by: Emil Sadek <esadek@users.noreply.github.com>

--- java/trino/src/main/java/tech/columnar/Example.java
+++ java/trino/src/main/java/tech/columnar/Example.java
@@ -16,41 +16,42 @@
 
 package tech.columnar;
 
-import org.apache.arrow.adbc.driver.jni.JniDriver;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.arrow.adbc.core.AdbcConnection;
 import org.apache.arrow.adbc.core.AdbcDatabase;
 import org.apache.arrow.adbc.core.AdbcStatement;
+import org.apache.arrow.adbc.driver.jni.JniDriver;
 import org.apache.arrow.adbc.drivermanager.AdbcDriverManager;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.vector.ipc.ArrowReader;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Example {
-    private static final String DRIVER_FACTORY = "org.apache.arrow.adbc.driver.jni.JniDriverFactory";
+  private static final String DRIVER_FACTORY = "org.apache.arrow.adbc.driver.jni.JniDriverFactory";
 
-    public static void main(String[] args) throws Exception {
-        Map<String, Object> params = new HashMap<>();
-        JniDriver.PARAM_DRIVER.set(params, "trino");
-        params.put("uri", "http://user@localhost:8080?catalog=tcph&schema=tiny");
+  public static void main(String[] args) throws Exception {
+    Map<String, Object> params = new HashMap<>();
+    JniDriver.PARAM_DRIVER.set(params, "trino");
+    params.put("uri", "http://user@localhost:8080?catalog=tcph&schema=tiny");
 
-        try (BufferAllocator allocator = new RootAllocator();
-             AdbcDatabase db = AdbcDriverManager.getInstance().connect(DRIVER_FACTORY, allocator, params);
-             AdbcConnection conn = db.connect();
-             AdbcStatement stmt = conn.createStatement()) {
-            stmt.setSqlQuery("""
-              SELECT nationkey, name, regionkey
-              FROM tpch.tiny.nation
-              LIMIT 5
-            """);
-            try (AdbcStatement.QueryResult result = stmt.executeQuery()) {
-                ArrowReader reader = result.getReader();
-                while (reader.loadNextBatch()) {
-                    System.out.println(reader.getVectorSchemaRoot().contentToTSVString());
-                }
-            }
+    try (BufferAllocator allocator = new RootAllocator();
+        AdbcDatabase db =
+            AdbcDriverManager.getInstance().connect(DRIVER_FACTORY, allocator, params);
+        AdbcConnection conn = db.connect();
+        AdbcStatement stmt = conn.createStatement()) {
+      stmt.setSqlQuery(
+          """
+            SELECT nationkey, name, regionkey
+            FROM tpch.tiny.nation
+            LIMIT 5
+          """);
+      try (AdbcStatement.QueryResult result = stmt.executeQuery()) {
+        ArrowReader reader = result.getReader();
+        while (reader.loadNextBatch()) {
+          System.out.println(reader.getVectorSchemaRoot().contentToTSVString());
         }
+      }
     }
+  }
 }
